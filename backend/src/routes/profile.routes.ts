687f15ce268f3d62fa3e6865cba6ca1f5b5b0cd2
daryloye <daryloye@gmail.com>
2026-02-19 19:03:25 +0800

    update profile routes

--- backend/src/routes/profile.routes.ts
+++ backend/src/routes/profile.routes.ts
@@ -1,12 +1,17 @@
-import { Router } from 'express'
-import { requireAuth } from '../middleware/auth.middleware';
-import { completeProfile, getOthersProfile, getOwnerProfile, updateOwnProfile } from '../controllers/profile.controller';
+import { Router } from "express";
+import {
+  completeProfile,
+  getOthersProfile,
+  getOwnerProfile,
+  updateOwnProfile,
+} from "../controllers/profile.controller";
+import { requireAuth } from "../middleware/auth.middleware";
 
 const router = Router();
 
-router.post('/complete-profile', requireAuth, completeProfile);
-router.get('/my-profile', requireAuth, getOwnerProfile);
-router.post('/update', requireAuth, updateOwnProfile);
-router.get('/:id', requireAuth, getOthersProfile);
+router.get("/me", requireAuth, getOwnerProfile); // /user ? to get name and profile pic
+router.get("/update", requireAuth, completeProfile); // to load Profile Page
+router.post("/update", requireAuth, updateOwnProfile); // to update Profile Page
+router.get("/:id", requireAuth, getOthersProfile);
 
 export default router;
