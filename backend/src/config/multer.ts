682f70946e1d6fa62546bd2c87ee85fad10ddb5e
jack <jack.sng@gmail.com>
2026-02-27 11:37:31 +0800

    setup multer for profile image uploads

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -12,7 +12,29 @@ import crypto from 'crypto';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/'); //callback
-            
+        cb(null, 'uploads/'); //callback: save ub uploads
     },
+    filename: (req, file, cb) => {
+        const extension = path.extname(file.originalname);
+        const uniqueName = crypto.randomBytes(16).toString('hex');
+        cb(null, `${uniqueName}${extension}`);
+    } 
 });
+
+const fileFilter = (req: any, file: Express.Multer.File, cb: any) => {
+    const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/webp'];
+
+    if(allowedTypes.includes(file.mimetype)){
+        cb(null, true);
+    } else {
+        cb(new Error('invalid file type. Only JPEG, PNG and WebP are allowed.'), false);
+    }
+};
+
+export const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+    }
+})
