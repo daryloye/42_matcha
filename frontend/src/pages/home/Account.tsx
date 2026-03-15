0fbff109961b596974aa3ebee66a0c2fcccace44
daryloye <daryloye@gmail.com>
2026-03-16 02:19:02 +0800

    add account page

--- frontend/src/pages/home/Account.tsx
+++ frontend/src/pages/home/Account.tsx
@@ -1,3 +1,4 @@
+import { List, VStack } from 'rsuite';
 import { HomePageTemplate } from './HomePageTemplate';
 
 export default function Account() {
@@ -5,5 +6,38 @@ export default function Account() {
 }
 
 function AccountPage() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Account</h1>
+
+      <div className='flex flex-col mt-5 gap-12'>
+        <VStack>
+          <p className='text-lg font-bold'>Your profile was viewed by:</p>
+          <List className='rounded-lg'>
+            {viewedByData.map((v, i) => (
+              <List.Item key={i} className='w-100 text-center'>
+                {v.username}
+              </List.Item>
+            ))}
+          </List>
+        </VStack>
+
+        <VStack>
+          <p className='text-lg font-bold'>Your profile was liked by:</p>
+          <List className='rounded-lg'>
+            {viewedByData.map((v, i) => (
+              <List.Item key={i} className='w-100 text-center'>
+                {v.username}
+              </List.Item>
+            ))}
+          </List>
+        </VStack>
+      </div>
+    </div>
+  );
 }
+
+const viewedByData = [
+  { id: 1, username: 'user1' },
+  { id: 2, username: 'user2' },
+];
